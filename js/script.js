90b79aca05e33c7fd7e4469944788badc39143d8
fox-dr <162760691+fox-dr@users.noreply.github.com>
2025-03-31 11:11:47 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -94,33 +94,38 @@ async function loadRegionData(region, config) {
       });
 
 if (layerType === 'circle') {
-  // Click handler for popups
   map.on('click', layerName, (e) => {
     const { geometry, properties } = e.features[0];
     const coords = geometry.coordinates;
 
-    let html = `<b>${properties.community || 'Unnamed'}</b><br>`;
+    let html = '';
 
-    if (properties.builder) html += `${properties.builder}<br>`;
-    if (properties.city && properties.state && properties.zip)
-      html += `${properties.city}, ${properties.state} ${properties.zip}<br>`;
-    if (properties.price_range) html += `${properties.price_range}<br>`;
-    if (properties.sf_range) html += `${properties.sf_range}<br>`;
+    if (layerName.includes('community')) {
+      html += `<b>${properties.community || 'Unnamed'}</b><br>`;
+      if (properties.builder) html += `${properties.builder}<br>`;
+      if (properties.city && properties.state && properties.zip)
+        html += `${properties.city}, ${properties.state} ${properties.zip}<br>`;
+      if (properties.price_range) html += `${properties.price_range}<br>`;
+      if (properties.sf_range) html += `${properties.sf_range}<br>`;
+    } else if (layerName.includes('portfolio')) {
+      html += `<b>${properties.name || 'Unnamed'}</b><br>`;
+      if (properties.description) html += `${properties.description}<br>`;
+    }
+
     if (properties.status) html += `<em>Status:</em> ${properties.status}`;
 
     new mapboxgl.Popup().setLngLat(coords).setHTML(html).addTo(map);
   });
 
-  // Mouse events (attach only once)
+  // Cursor events
   map.on('mouseenter', layerName, () => {
     map.getCanvas().style.cursor = 'pointer';
   });
-
   map.on('mouseleave', layerName, () => {
     map.getCanvas().style.cursor = '';
   });
 }
-    
+ 
 
       console.log('Loaded layer:', layerName);
     } catch (e) {
