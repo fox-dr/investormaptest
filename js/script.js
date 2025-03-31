012102cb9f44bc5443b62d966e466cba128f0d9a
fox-dr <162760691+fox-dr@users.noreply.github.com>
2025-03-30 23:43:13 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -80,7 +80,6 @@ async function loadRegionData(region, config) {
       type: layerType,
       source: layerName,
       layout: layerType === 'symbol' ? {
-        'icon-image': 'marker-default',
         'icon-allow-overlap': true,
         'icon-anchor': 'bottom',
       } : {}, // Empty layout for other types
